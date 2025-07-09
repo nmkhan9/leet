442b83fca63e7bb92777a5c6576b6ea78bfab9bc
Nguyen Manh Khang <nguyenmanhkhanghd@gmail.com>
2025-07-09 10:03:03 +0700

    Time: 7 ms (6.15%), Space: 12.5 MB (8.14%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+
+        while num >= 10 :
+            num = sum(int(x) for x in str(num) )
+        return num
+
+        
